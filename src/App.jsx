65dd259cf79099a8987db2d812917b2fff76ba19
Carlin Dsouza <142723566+carlinidk@users.noreply.github.com>
2026-01-05 02:02:55 +0530

    Create App component with main layout structure

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import Navbar from "./components/Navbar"
+import Hero from "./components/Hero"
+import Products from "./components/Products"
+import Quote from "./components/Quote"
+import Gallery from "./components/Gallery"
+import QnA from "./components/QnA"
+import Footer from "./components/Footer"
+
+export default function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Products />
+      <Quote />
+      <Gallery />
+      <QnA />
+      <Footer />
+    </>
+  )
+}
